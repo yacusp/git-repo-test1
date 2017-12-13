3dd5f6428d41f5ba5f6bf68dfc2893ce77cba8e6
yawolf <yawolf@mail.ru>
2017-12-13 15:57:47 +0300

    Comments edited

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,16 +1,5 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
+  Blink one LED
  */
 
 
